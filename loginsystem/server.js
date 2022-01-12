bedce18a272537b4b9e7ce73b2aa7adc803bf0e3
ranamalsingh12 <ranams99911@gmail.com>
2022-01-12 14:57:48 +0530

    login

--- loginsystem/server.js
+++ loginsystem/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000
 
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+
+//load static asset
+app.use('/static', express.static( path.join(__dirname,'Public')));
+app.use('/assets', express.static(path.join(__dirname,'Public/assets')))
 
 //home route
 app.get('/',(req, res) => {
