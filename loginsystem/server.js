4aebc212814d40f59ebb1dd0828c4453681950a5
ranamalsingh12 <ranams99911@gmail.com>
2022-01-12 15:09:31 +0530

    login

--- loginsystem/server.js
+++ loginsystem/server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const path = require('path');
+const bodyparser = require('body-parser');
+const session = require("express-session");
 const app = express();
+const {v4: uuidv4} = require("uuid")
 
 const PORT = process.env.PORT || 3000
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}))
 
 app.set("view engine","ejs");
 
 //load static asset
 app.use('/static', express.static( path.join(__dirname,'Public')));
 app.use('/assets', express.static(path.join(__dirname,'Public/assets')))
+
+app.use(session({
+    secret : uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
 
 //home route
 app.get('/',(req, res) => {
