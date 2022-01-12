cc0e60fa0f568257f3dfc389916ff35adac4ebf1
ranamalsingh12 <ranams99911@gmail.com>
2022-01-12 10:57:03 +0530

    cookies

--- cookies.js
+++ cookies.js
@@ -1,1 +1,30 @@
 const express = require('express')
+const app = express();
+const cookies = require('cookie-parser')
+
+const PORT = process.env.PORT || 3000
+
+app.use(cookies());
+
+let users = {
+    name:"John",
+    age: 29
+}
+
+app.get('/',(req, res ) => {
+    res.send("Cookies")
+})
+
+app.get('/setUser',(req, res ) => {
+    res.cookie("userData",users)
+    res.send("userdata added to cookies");
+})
+
+
+app.get('/getUser',(req, res ) => {
+    res.send(req.cookies)
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening the request to http://localhost:${PORT}`);
+})
