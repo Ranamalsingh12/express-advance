da01a95d79b1567639281f813f0665e06b610cbc
ranamalsingh12 <ranams99911@gmail.com>
2022-01-14 22:07:48 +0530

    morgan logger

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3000;
 
+app.get('/',(req, res)=>{
+    res.end("morgan logger")
+})
+
 app.listen(3000,() => {
     console.log(`Listening on http://localhost${port}`);
 })
