b33f314870a5c95977d9a1216be04f251499c712
ranamalsingh12 <ranams99911@gmail.com>
2022-01-15 11:17:39 +0530

    creating the uuid

--- morgan-app/server.js
+++ morgan-app/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const {v4 : uuidv4} = require("uuid");
 const app = express();
+
+
 
 const port = 3000;
 
-app.use(morgan(':method :status'));
-
+app.use(morgan(':method '));
 
 app.get('/',(req, res)=>{
     res.end("morgan logger")
 })
+
+function assignId(req, res) {
+    req.id = uuid4();
+    next();
+}
 
 app.listen(3000,() => {
     console.log(`Listening on http://localhost${port}`);
