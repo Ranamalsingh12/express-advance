c5f9639f693ea85e33e661ce952a99b06bc91ca3
ranamalsingh12 <ranams99911@gmail.com>
2022-01-15 12:17:24 +0530

    morgan id

--- morgan-app/server.js
+++ morgan-app/server.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
-const {v4 : uuidv4} = require("uuid");
+const { v4 } = require("uuid");
 const app = express();
-
-
 
 const port = 3000;
 
-app.use(morgan(':method '));
+morgan.token('id',function getId(req) {
+    return req.id;
+});
+
+morgan.token('mayank',function (req,res,mayank) {
+    return "user id"
+})
+
+app.use(assignId);
+
+app.use(morgan(':id :mayank :url :response-time"'));
 
 app.get('/',(req, res)=>{
     res.end("morgan logger")
 })
 
-function assignId(req, res) {
-    req.id = uuid4();
+function assignId(req, res, next) {
+    req.id = v4();
+    console.log('id ', req.id); 
     next();
 }
 
